0a1029f118160911db4ab9bf12191dd352c4054c
Han Sol Jin <hansol@hansol.ca>
2024-07-29 21:55:44 -0700

    joan: Move to android-11

--- twrp_joan.mk
+++ twrp_joan.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := joan
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
@@ -13,7 +13,6 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := joan
-PRODUCT_MODEL := LG-V30
-PRODUCT_NAME := omni_joan
+PRODUCT_NAME := twrp_joan
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
